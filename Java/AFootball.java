ff60b3dcbb828fc2b85131268ee80d5f1eafb19b
Chandan Prajwal <76419264+ChandanSouL@users.noreply.github.com>
2024-11-09 15:31:07 +0530

    09/11/24

--- Java/AFootball.java
+++ Java/AFootball.java
@@ -1,10 +1,15 @@
-package Java;
 import java.util.Scanner;
 
 public class AFootball {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
         String t = n.next();
+
+        if(t.contains("0000000") || t.contains("1111111")){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
         
     }
 }
